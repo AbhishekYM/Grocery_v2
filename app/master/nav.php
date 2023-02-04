242e396122de1fd2f003aca8c22ceb7f1e1ef358
Abhishek Makwana <makwanaabhishek843@gmail.com>
2023-02-04 18:34:04 +0530

    changes in product details page

--- app/master/nav.php
+++ app/master/nav.php
@@ -3,7 +3,8 @@ include('D:/xampp/htdocs/Grocery/database/connection.php');
 ?>
 <!----======== CSS ======== -->
 <link rel="stylesheet" href="style.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" -->
     integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 <!----===== Iconscout CSS ===== -->
 <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
--- html/master/nav.php
+++ html/master/nav.php
@@ -12,9 +12,7 @@ include('D:/xampp/htdocs/Grocery/database/connection.php');
 	<!--Code for linking css file-->
 	<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 	<link rel="icon" type="image/x-icon" class="fa fa-shopping-basket" href="">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
-    integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
-    crossorigin="anonymous" />
+	
 
 <!--Header Section-->
 <header class="header">
